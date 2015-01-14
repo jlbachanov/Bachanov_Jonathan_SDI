1969c3a57bc1f0fa7074fe96d60fcace458ac991
jlbachanov <JonathanBachanov@jonathans-mbp.pk5001z>
2015-01-14 15:28:45 -0500

    declared variables
    declared input variables and total.

--- Day 4/js/day4_Expressions.js
+++ Day 4/js/day4_Expressions.js
@@ -8,7 +8,6 @@ Expressions Assignment
 //alert ("Testing to see if it works");
 // Gather the prices of lunch
  var userInput= prompt("How much you spend on lunch on Monday?");
-
  var userInput1= prompt("How much you spend on lunch on Tuesday?");
  var userInput2= prompt("How much you spend on lunch on Wednesday?");
  var userInput3= prompt("How much you spend on lunch on Thursday?");
@@ -21,9 +20,11 @@ Expressions Assignment
 
  var userInputs = [userInput, userInput1, userInput2, userInput3, userInput4];
 
- userInputs[0] +=   userInputs[1] += userInputs[2] += userInputs[3] +=userInputs[4];
+ userInputs[0] +=   userInputs[1] += userInputs[2] += userInputs[3] += userInputs[4];
 
-
+ var total = userInputs[0];
+ var nTotal = total/5;
+ console.log(nTotal);
 
 
 
