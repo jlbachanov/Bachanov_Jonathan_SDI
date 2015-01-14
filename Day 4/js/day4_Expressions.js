5abc6597931381dc95f0c2657bcac085661b3a2e
jlbachanov <JonathanBachanov@Jonathans-MacBook-Pro.local>
2015-01-14 00:01:58 -0500

    day 4 expressions
    declaring variables to set up equation

---
+++ Day 4/js/day4_Expressions.js
@@ -0,0 +1,16 @@
+
+ /*
+Jonathan Bachanov
+SDI Campus
+1/13/2015
+Expressions Assignment
+ */
+//alert ("Testing to see if it works");
+// Calculate monthly spending money
+
+ var dadIncome = prompt("Please enter the husbands monthly income:");
+ var wifeIncome= prompt("Please enter the wives monthly income:");
+ var expenses= prompt("Please enter your household monthly expenses");
+ var totalIncome= dadIncome + wifeIncome
+ var spendingMoney= totalIncome-expenses
+ 
