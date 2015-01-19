bb49cc1748b5db220b0c015d25b6a26e7fb22952
jlbachanov <JonathanBachanov@jonathans-mbp.pk5001z>
2015-01-19 18:08:34 -0500

    commented out code
    went over code and commented out correctly

--- Bachanov_Jonathan_Conditionals_Assignment/Bachanov_Jonathan_Conditionals_Assignment.js
+++ Bachanov_Jonathan_Conditionals_Assignment/Bachanov_Jonathan_Conditionals_Assignment.js
@@ -19,25 +19,25 @@ var flight= prompt("Assuming your budget is $2,000 for vacation, how much would 
 
 
  if  (flight==="")  {flight1=
-  prompt("How much did you want to spend on a flight?");}
+  prompt("How much did you want to spend on a flight?");} // if statement that will re ask prompt.
 
  if (hotel==="") { hotel1=
-  prompt("How much is the cost on your hotel?");}
+  prompt("How much is the cost on your hotel?");}// if statement that will re ask prompt.
 
  if (activities===("")){var activities1=
-  prompt("How much would you spend for the activities?");}
+  prompt("How much would you spend for the activities?");}//if statement that will re ask prompt
 
- activities= parseInt(activities)||parseInt(activities1);
- flight= parseInt(flight)|| parseInt(flight1);
- hotel= parseInt(hotel) || parseInt(hotel1);
+ activities= parseInt(activities)||parseInt(activities1);// taking out integer "or" statement to define value of activities.
+ flight= parseInt(flight)|| parseInt(flight1);// taking out integer from "or" statement to define flight
+ hotel= parseInt(hotel) || parseInt(hotel1);// taking out integer from "or" statement to define hotel.
 
- total= activities + flight + hotel;
+ total= activities + flight + hotel;// declare and define the value of the new variable total. make it easier to write ternary.
 
- total2= (total<=2000)? ("You have enough money to go on vacation and do all the activities.") :
-     (total-activities <= 2000) ? ("You have enough money to go on vacation, but can't do any activities") :
-         ("You are to broke and can't go on vacation.");
+ total2= (total<=2000)? ("You have enough money to go on vacation and do all the activities.") :// ternary if total<= 2,000 then "".
+     (total-activities <= 2000) ? ("You have enough money to go on vacation, but can't do any activities") ://ternary else if  total minus activities <= 2000 then ""
+         ("You are to broke and can't go on vacation.");// else you cant go
 
- console.log(total2);
+ console.log(total2); /// outputing the total2.
 
 
 
