01a447db117190e7055887da84bd7eaf8555b11d
jlbachanov <JonathanBachanov@Jonathans-MacBook-Pro.local>
2015-01-20 18:50:54 -0500

    loops and break command
    learned how create loops and use the break command to stop code.

--- Day 7/js/day7_forloops.js
+++ Day 7/js/day7_forloops.js
@@ -4,4 +4,24 @@
  *for loops
  *
  **/
-alert("testing");
+//alert("testing");
+
+// basic for loop structure
+/*
+
+for(initialization; condition to test; increment of change;){  code to run as long as condition is true.
+    
+    
+}*/
+
+for(var i= 0 ; i<20; i+=4) {
+    console.log ("The value of I = " + i);
+}
+
+for(var k= 0; k<5; k++){
+    
+    console.log("The value of K =" + k);
+if (k===3) {break;
+    //code
+}
+}
