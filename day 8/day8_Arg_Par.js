849c47ae3ea60ba252ce88b41f63a7aa04d10a4f
jlbachanov <JonathanBachanov@Jonathans-MacBook-Pro.local>
2015-01-22 20:03:39 -0500

    day 8 return
    made html and js for day 8 return files

--- day 8/day8_Arg_Par.js
+++ day 8/day8_Arg_Par.js
@@ -1,1 +1,41 @@
-alert("tesfd");
+/*
+ * Jonathan Bachanov
+ * 1/22/2015
+ * Arguments and Parameters
+ */
+
+// Arguments - Goes in the function call
+//Parameters - Goes in the function defintion
+// must have the same number of argument and parameters usually.
+// create a function to calculate the area of a rectangle
+
+function calcArea(w,h){//parameters go here
+    
+    //var width= 10;
+    //var height= 20;
+    //var area= width*height;
+    
+    var area= w*h
+    console.log ("the area is " + area)
+    
+    
+}
+// function call
+calcArea(10,20);
+calcArea(5,4);
+
+
+// calculate dog years
+// put in human years, get out dog years
+
+function dogYears(humanAge) {   // humanAge is a parameter
+    /// dog year = human*7
+    var dogAge= humanAge*7;
+    console.log("Dog age is " + dogAge);
+    //code
+}
+dogYears(25);
+
+var userAge= prompt("How old are you?");
+
+dogYears(Number(userAge));
